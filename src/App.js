03cbff6e3e8e79aa12d7bfb126d319b24dbd9429
felixgyamfi <felix.gyamfi.design@gmail.com>
2021-04-21 19:07:34 +0200

    build Browse

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import GlobalStyle from './themes/globalStyles';
 import Dashboard from './components/Dashboard/Dashboard';
+import Browse from './components/Browse/Browse';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Dashboard />
+      <Browse />
       <h1>App</h1>
     </div>
   );
---
+++ src/components/Browse/Browse.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Browse() {
+  return (
+    <div className="Browse">
+      <h1>Browse</h1>
+    </div>
+  );
+}
+
+export default Browse;
