2b24bd5100749ecec5df515ca3fdbb048fa33b3f
felixgyamfi <felix.gyamfi.design@gmail.com>
2021-04-22 17:39:46 +0200

    start dev branch for build up

---
+++ src/components/Dashboard/Content/Content.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import GlobalStyle from '../../../themes/globalStyles';
+
+function Content() {
+  return (
+    <div>
+      <GlobalStyle />
+      <h2>Dein Workout heute</h2>
+      <div>Trainingsplan</div>
+    </div>
+  );
+}
+
+export default Content;
--- src/themes/globalStyles.js
+++ src/themes/globalStyles.js
@@ -17,17 +17,17 @@ const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         background: #FFFFFF;
-        font-family: Open-Sans, Monospace, Sans-Serif;
+        font-family: Montserrat;
         font-size: calc(10px + 1vw);
     }
 
     h1 {
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        font-size: 3.6em;
+        font-size: 3.6rem;
+        color: #1D2A73;
+    }
+
+    h2 {
+        font-size: 2.4rem;
         color: #1D2A73;
     }
 `;
