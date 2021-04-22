794083fcafd299236e53f9191aff6a38c0fae531
felixgyamfi <felix.gyamfi.design@gmail.com>
2021-04-22 17:37:57 +0200

    save changes and create dev branch

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import GlobalStyle from '../../themes/globalStyles';
+import styled from 'styled-components';
+
+const WrapperNav = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  background: #f5ebdd;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 5rem;
+  padding: 1rem 5rem 0.2rem;
+`;
+
+const WrapperLink = styled.div`
+  display: flex;
+  color: #1d2a73;
+  font-size: 1rem;
+  letter-spacing: 0.2rem;
+`;
 
 function NavBar() {
   return (
-    <div className="NavBar">
-      <GlobalStyle />
-      <h1>NavBar</h1>
-    </div>
+    <WrapperNav>
+      <WrapperLink>HOME</WrapperLink>
+      <WrapperLink>BROWSE</WrapperLink>
+      <WrapperLink>PROFIL</WrapperLink>
+    </WrapperNav>
   );
 }
 
