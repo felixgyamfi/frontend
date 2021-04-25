0c9260ed18cfffe5f395c2d5565aba05626eecf4
felixgyamfi <felix.gyamfi.design@gmail.com>
2021-04-26 01:16:18 +0200

    atomic design code refracture #1

--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Content from '../../organisms/DailyWorkout/index.js';
-import { WelcomeHead } from '../../atoms/WelcomeHead';
+import Content from '../../organisms/dailyWorkout/index.js';
+import { WelcomeHead } from '../../atoms/header/WelcomeHead';
 
 const WrapperDash = styled.div`
   padding: 0 1.7rem;
