ae670e81c347bdd08d3014f72c4bab95824b6db5
felixgyamfi <felix.gyamfi.design@gmail.com>
2021-05-02 20:40:47 +0200

    data routing to detail page

--- src/components/pages/Browse/mapContentCards.js
+++ src/components/pages/Browse/mapContentCards.js
@@ -2,9 +2,9 @@ import Data from '../../atoms/Data/ProgramData';
 import ProgramCard from '../../molecules/ProgramCard/index';
 
 const mapProgram = () => {
-  const a = Data.map(({ type, title }) => (
+  const a = Data.map(({ type, title, id }) => (
     <>
-      <ProgramCard type={type} title={title} />
+      <ProgramCard type={type} title={title} id={id} />
     </>
   ));
   return <div>{a}</div>;
