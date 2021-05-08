914718b0944fcb9038bf376207f324fadda71d58
felixgyamfi <felix.gyamfi.design@gmail.com>
2021-05-08 15:27:41 +0200

    fix CloseButton Link area

--- src/components/atoms/Buttons/CloseButton/CloseButton.js
+++ src/components/atoms/Buttons/CloseButton/CloseButton.js
@@ -2,7 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import CloseIcon from './assets/icon--x_close.png';
 
-export const Link = styled.a`
+export const Link = styled.a``;
+
+export const Wrapper = styled.div`
   display: flex;
   justify-content: flex-end;
   width: 100%;
@@ -10,9 +12,11 @@ export const Link = styled.a`
 
 function CloseButton({ href }) {
   return (
-    <Link href={href}>
-      <img src={CloseIcon} alt="Close Button" />
-    </Link>
+    <Wrapper>
+      <Link href={href}>
+        <img src={CloseIcon} alt="Close Button" />
+      </Link>
+    </Wrapper>
   );
 }
 
